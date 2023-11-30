8ccad911bcf41c85c8229b070a83f1193a76c8a1
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-30 12:29:55 +0100

    CargaScripts 11

--- CargaScripts.cpp
+++ CargaScripts.cpp
@@ -20,57 +20,24 @@ ConsoleBox *consoleBox = new ConsoleBox;
 void load_script(const char* filename, bool show_script = false)
 {
     string script;
-    FILE* f = nullptr;
-    try
-    {
-        f = fopen(filename, "rb");
-        if (!f)
-        {
-            cerr << "Error de apertura de " << filename << endl;
-            return;
-        }
+    ifstream file(filename);
 
-        int c;
-        char buf[4001];
-        while ((c = fread(buf, 1, 4000, f)) > 0)
-        {
-            buf[c] = 0;
-            script.append(buf);
+    if (!file) {
+        if (file.fail()) {
+            cerr << "Error: Fallo en la operación de apertura del archivo " << filename << endl;
+        } else {
+            cerr << "Error: El archivo " << filename << " no existe." << endl;
         }
-        fclose(f);
-        f = nullptr;
-        if (show_script)
-        {
-            cout << ColorConsole::fg_blue << ColorConsole::bg_white;
-            cout << script << endl;
-        }
-        consoleBox->new_text();
-        consoleBox->set_text(script);
+        return;
     }
-    catch (const std::exception& e)
-    {
-        cerr << "Error durante la lectura del archivo: " << e.what() << endl;
-        if (f)
-            fclose(f);
-    }
-    catch (...)
-    {
-        cerr << "Error desconocido durante la lectura del archivo" << endl;
-        if (f)
-            fclose(f);
-    }
-}
-void load_script()
-{
-    char filename[500];
-    printf("Archivo: ");
-    scanf("%499s", filename);
-    load_script(filename, true);
-}
 
-int main()
-{
-    load_script();
+    script.assign((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+    file.close();
 
-    return 0;
+    if (show_script) {
+        cout << ColorConsole::fg_blue << ColorConsole::bg_white;
+        cout << script << endl;
+    }
+    consoleBox->new_text();
+    consoleBox->set_text(script);
 }
