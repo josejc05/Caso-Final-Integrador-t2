2993ce9c20f83120ba3e34a1f7cab0837f20b39b
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-30 12:06:07 +0100

    CargaScripts 6

--- CargaScripts.cpp
+++ CargaScripts.cpp
@@ -17,3 +17,25 @@ struct ConsoleBox
 };
 
 ConsoleBox *consoleBox = new ConsoleBox;
+void load_script(const char* filename, bool show_script = false)
+{
+    string script;
+    FILE* f = nullptr;
+    try
+    {
+        f = fopen(filename, "rb");
+        if (!f)
+        {
+            cerr << "Error de apertura de " << filename << endl;
+            return;
+        }
+
+        int c;
+        char buf[4001];
+        while ((c = fread(buf, 1, 4000, f)) > 0)
+        {
+            buf[c] = 0;
+            script.append(buf);
+        }
+        fclose(f);
+        f = nullptr;
