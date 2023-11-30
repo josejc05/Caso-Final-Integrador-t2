5e09e6fd467eb299751427804428e76cfcf9879c
Jose Jurado Cortés <jjuracor@myuax.com>
2023-11-30 11:51:29 +0100

    CargaScripts 2

--- CargaScripts.cpp
+++ CargaScripts.cpp
@@ -15,3 +15,15 @@ void load_script(const char* filename, bool show_script = false) {
         cerr << "Error: No se pudo abrir el archivo " << filename << endl;
         return;
     }
+    string script((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+    file.close();
+
+    if (show_script) {
+        cout << "Contenido del archivo '" << filename << "':" << endl;
+        cout << script << endl;
+    } else {
+        cout << "Archivo '" << filename << "' cargado en la consola." << endl;
+    }
+
+    apply_syntax_coloring(script);
+}
